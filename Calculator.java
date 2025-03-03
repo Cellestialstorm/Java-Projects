fc7fa8603f0eac33b321e544f7813d134a6a2adb
Rohan Sharma <rohankhatiwara76@gmail.com>
2025-03-03 21:39:53 +0530

    Basic Calculator

--- Calculator.java
+++ Calculator.java
@@ -3,67 +3,57 @@ import java.util.Scanner;
 public class Calculator {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int inp = 0;
-		int num1, num2;
-		while (inp != 5) {
-			System.out.println("\nSelect the operation you want to perform:\n1 for multiplication\n2 for Addition\n3 for Substraction\n4 for Division\n5 to exit\n");
+		int choice;
+		do {
+			System.out.println("\nSelect the operation you want to perform: ");
+			System.out.println("1 for Addition");
+			System.out.println("2 for Substraion");
+			System.out.println("3 for Multiplication");
+			System.out.println("4 for Division");
+			System.out.println("5 to Exit");
 			System.out.print("Enter your choice: ");
-			inp = scan.nextInt();
+			
+			choice = scan.nextInt();
 
-			if (inp == 5) {
-				System.out.println("Thank you for uisng this calculator");
-				break;
+			if (choice >= 1 && choice <= 4) {
+				System.out.print("\nEnter your first number: ");
+				int num1 = scan.nextInt();
+
+				System.out.print("Enter your second number: ");
+				int num2 = scan.nextInt();
+				
+				performAction(choice, num1, num2);
+			} else if (choice != 5) {
+				System.out.println("\nEnter a valid choice!!");
 			}
+		} while (choice != 5);
 
-			switch(inp) {
-				case 1: 
-					System.out.print("Enter your first number: ");
-					num1 = scan.nextInt();
-
-					System.out.print("Enter your second number: ");
-					num2 = scan.nextInt();
-
-					System.out.println("\nMultiplication of these numbers is " + (num1 * num2));
-					break;
-
-				case 2: 
-					System.out.print("Enter your first number: ");
-					num1 = scan.nextInt();
-
-					System.out.print("Enter your second number: ");
-					num2 = scan.nextInt();
-
-					System.out.println("\nAddition of these numbers is " + (num1 + num2));
-					break;
-
-				case 3:
-					System.out.print("Enter your first number: ");
-					num1 = scan.nextInt();
-
-					System.out.print("Enter your second number: ");
-					num2 = scan.nextInt();
-
-					System.out.println("\nSubtraction of these numbers is " + (num1 - num2));
-					break;
-
-				case 4: 
-					System.out.print("Enter your first number: ");
-					num1 = scan.nextInt();
-
-					System.out.print("Enter your second number: ");
-					num2 = scan.nextInt();
-
-					if (num2 == 0) {
-						System.out.println("Can't divide by zero");
-						break;
-					} else {
-						System.out.printf("\nDivision of these numbers is %.2f%n", ((double)num1 / num2));
-						break;
-					}
-
-				default: System.out.println("\nPlease select a valid operation");
-			}
-		}
+		System.out.println("\nThank you for using this calculator");
 		scan.close();
+	}
+
+	private static void performAction(int choice, int num1, int num2) {
+		switch(choice) {
+			case 1: 
+				System.out.println("\nMultiplication of these numbers is " + (num1 * num2));
+				break;
+
+			case 2: 
+				System.out.println("\nAddition of these numbers is " + (num1 + num2));
+				break;
+
+			case 3:
+				System.out.println("\nSubtraction of these numbers is " + (num1 - num2));
+				break;
+
+			case 4: 
+				if (num2 == 0) {
+					System.out.println("Can't divide by zero");
+					break;
+				} else {
+					System.out.printf("\nDivision of these numbers is %.2f%n", ((double)num1 / num2));
+					break;
+				}
+		}
 	}
 } 
